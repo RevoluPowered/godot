9bfc5607cf8ac1d038afde341d582cc3ec39d323
Gordon MacPherson <gordon@gordonite.tech>
2019-08-13 15:38:07 +0100

    Refactoring all tools into import_utils.h
    Trying to decrease technical debt

--- modules/assimp/editor_scene_importer_assimp.cpp
+++ modules/assimp/editor_scene_importer_assimp.cpp
@@ -54,6 +54,7 @@
 #include "scene/resources/material.h"
 #include "scene/resources/surface_tool.h"
 #include "zutil.h"
+#include "import_utils.h"
 #include <string>
 
 void EditorSceneImporterAssimp::get_extensions(List<String> *r_extensions) const {
@@ -586,40 +587,6 @@ void EditorSceneImporterAssimp::_import_animation(ImportState &state, int p_anim
 	}
 }
 
-float EditorSceneImporterAssimp::_get_fbx_fps(int32_t time_mode, const aiScene *p_scene) {
-	switch (time_mode) {
-		case AssetImportFbx::TIME_MODE_DEFAULT: return 24; //hack
-		case AssetImportFbx::TIME_MODE_120: return 120;
-		case AssetImportFbx::TIME_MODE_100: return 100;
-		case AssetImportFbx::TIME_MODE_60: return 60;
-		case AssetImportFbx::TIME_MODE_50: return 50;
-		case AssetImportFbx::TIME_MODE_48: return 48;
-		case AssetImportFbx::TIME_MODE_30: return 30;
-		case AssetImportFbx::TIME_MODE_30_DROP: return 30;
-		case AssetImportFbx::TIME_MODE_NTSC_DROP_FRAME: return 29.9700262f;
-		case AssetImportFbx::TIME_MODE_NTSC_FULL_FRAME: return 29.9700262f;
-		case AssetImportFbx::TIME_MODE_PAL: return 25;
-		case AssetImportFbx::TIME_MODE_CINEMA: return 24;
-		case AssetImportFbx::TIME_MODE_1000: return 1000;
-		case AssetImportFbx::TIME_MODE_CINEMA_ND: return 23.976f;
-		case AssetImportFbx::TIME_MODE_CUSTOM:
-			int32_t frame_rate;
-			p_scene->mMetaData->Get("FrameRate", frame_rate);
-			return frame_rate;
-	}
-	return 0;
-}
-
-Transform EditorSceneImporterAssimp::_get_global_assimp_node_transform(const aiNode *p_current_node) {
-	aiNode const *current_node = p_current_node;
-	Transform xform;
-	while (current_node != NULL) {
-		xform = _assimp_matrix_transform(current_node->mTransformation) * xform;
-		current_node = current_node->mParent;
-	}
-	return xform;
-}
-
 Ref<Texture> EditorSceneImporterAssimp::_load_texture(ImportState &state, String p_path) {
 	Vector<String> split_path = p_path.get_basename().split("*");
 	if (split_path.size() == 2) {
@@ -688,6 +655,10 @@ Ref<Texture> EditorSceneImporterAssimp::_load_texture(ImportState &state, String
 	return p_texture;
 }
 
+
+/** Why do we need so much material code?
+ *  400 lines of material code - technical debt
+ */
 Ref<Material> EditorSceneImporterAssimp::_generate_material_from_index(ImportState &state, int p_index, bool p_double_sided) {
 
 	ERR_FAIL_INDEX_V(p_index, (int)state.assimp_scene->mNumMaterials, Ref<Material>());
@@ -1087,7 +1058,9 @@ Ref<Material> EditorSceneImporterAssimp::_generate_material_from_index(ImportSta
 
 	return mat;
 }
-
+//
+// Mesh Generation from indicies ? why do we need so much mesh code
+// [debt needs looked into]
 Ref<Mesh> EditorSceneImporterAssimp::_generate_mesh_from_surface_indices(ImportState &state, Transform *parent_node, const Vector<int> &p_surface_indices, Skeleton *p_skeleton, bool p_double_sided_material) {
 
 	Ref<ArrayMesh> mesh;
@@ -1491,148 +1464,4 @@ void EditorSceneImporterAssimp::_generate_node(ImportState &state, const aiNode 
 	for (size_t i = 0; i < p_assimp_node->mNumChildren; i++) {
 		_generate_node(state, p_assimp_node->mChildren[i], new_node);
 	}
-}
-
-void EditorSceneImporterAssimp::_calc_tangent_from_mesh(const aiMesh *ai_mesh, int i, int tri_index, int index, PoolColorArray::Write &w) {
-	const aiVector3D normals = ai_mesh->mAnimMeshes[i]->mNormals[tri_index];
-	const Vector3 godot_normal = Vector3(normals.x, normals.y, normals.z);
-	const aiVector3D tangent = ai_mesh->mAnimMeshes[i]->mTangents[tri_index];
-	const Vector3 godot_tangent = Vector3(tangent.x, tangent.y, tangent.z);
-	const aiVector3D bitangent = ai_mesh->mAnimMeshes[i]->mBitangents[tri_index];
-	const Vector3 godot_bitangent = Vector3(bitangent.x, bitangent.y, bitangent.z);
-	float d = godot_normal.cross(godot_tangent).dot(godot_bitangent) > 0.0f ? 1.0f : -1.0f;
-	Color plane_tangent = Color(tangent.x, tangent.y, tangent.z, d);
-	w[index] = plane_tangent;
-}
-
-void EditorSceneImporterAssimp::_set_texture_mapping_mode(aiTextureMapMode *map_mode, Ref<Texture> texture) {
-	ERR_FAIL_COND(map_mode == NULL);
-	aiTextureMapMode tex_mode = aiTextureMapMode::aiTextureMapMode_Wrap;
-	//for (size_t i = 0; i < 3; i++) {
-	tex_mode = map_mode[0];
-	//}
-	int32_t flags = Texture::FLAGS_DEFAULT;
-	if (tex_mode == aiTextureMapMode_Wrap) {
-		//Default
-	} else if (tex_mode == aiTextureMapMode_Clamp) {
-		flags = flags & ~Texture::FLAG_REPEAT;
-	} else if (tex_mode == aiTextureMapMode_Mirror) {
-		flags = flags | Texture::FLAG_MIRRORED_REPEAT;
-	}
-	texture->set_flags(flags);
-}
-
-void EditorSceneImporterAssimp::_find_texture_path(const String &r_p_path, String &r_path, bool &r_found) {
-
-	_Directory dir;
-
-	List<String> exts;
-	ImageLoader::get_recognized_extensions(&exts);
-
-	Vector<String> split_path = r_path.get_basename().split("*");
-	if (split_path.size() == 2) {
-		r_found = true;
-		return;
-	}
-
-	if (dir.file_exists(r_p_path.get_base_dir() + r_path.get_file())) {
-		r_path = r_p_path.get_base_dir() + r_path.get_file();
-		r_found = true;
-		return;
-	}
-
-	for (int32_t i = 0; i < exts.size(); i++) {
-		if (r_found) {
-			return;
-		}
-		if (r_found == false) {
-			_find_texture_path(r_p_path, dir, r_path, r_found, "." + exts[i]);
-		}
-	}
-}
-
-void EditorSceneImporterAssimp::_find_texture_path(const String &p_path, _Directory &dir, String &path, bool &found, String extension) {
-	String name = path.get_basename() + extension;
-	if (dir.file_exists(name)) {
-		found = true;
-		path = name;
-		return;
-	}
-	String name_ignore_sub_directory = p_path.get_base_dir().plus_file(path.get_file().get_basename()) + extension;
-	if (dir.file_exists(name_ignore_sub_directory)) {
-		found = true;
-		path = name_ignore_sub_directory;
-		return;
-	}
-
-	String name_find_texture_sub_directory = p_path.get_base_dir() + "/textures/" + path.get_file().get_basename() + extension;
-	if (dir.file_exists(name_find_texture_sub_directory)) {
-		found = true;
-		path = name_find_texture_sub_directory;
-		return;
-	}
-	String name_find_texture_upper_sub_directory = p_path.get_base_dir() + "/Textures/" + path.get_file().get_basename() + extension;
-	if (dir.file_exists(name_find_texture_upper_sub_directory)) {
-		found = true;
-		path = name_find_texture_upper_sub_directory;
-		return;
-	}
-	String name_find_texture_outside_sub_directory = p_path.get_base_dir() + "/../textures/" + path.get_file().get_basename() + extension;
-	if (dir.file_exists(name_find_texture_outside_sub_directory)) {
-		found = true;
-		path = name_find_texture_outside_sub_directory;
-		return;
-	}
-
-	String name_find_upper_texture_outside_sub_directory = p_path.get_base_dir() + "/../Textures/" + path.get_file().get_basename() + extension;
-	if (dir.file_exists(name_find_upper_texture_outside_sub_directory)) {
-		found = true;
-		path = name_find_upper_texture_outside_sub_directory;
-		return;
-	}
-}
-
-String EditorSceneImporterAssimp::_assimp_get_string(const aiString &p_string) const {
-	//convert an assimp String to a Godot String
-	String name;
-	name.parse_utf8(p_string.C_Str() /*,p_string.length*/);
-	if (name.find(":") != -1) {
-		String replaced_name = name.split(":")[1];
-		print_verbose("Replacing " + name + " containing : with " + replaced_name);
-		name = replaced_name;
-	}
-
-	name = name.replace(".", ""); //can break things, specially bone names
-
-	return name;
-}
-
-String EditorSceneImporterAssimp::_assimp_anim_string_to_string(const aiString &p_string) const {
-
-	String name;
-	name.parse_utf8(p_string.C_Str() /*,p_string.length*/);
-	if (name.find(":") != -1) {
-		String replaced_name = name.split(":")[1];
-		print_verbose("Replacing " + name + " containing : with " + replaced_name);
-		name = replaced_name;
-	}
-	return name;
-}
-
-String EditorSceneImporterAssimp::_assimp_raw_string_to_string(const aiString &p_string) const {
-	String name;
-	name.parse_utf8(p_string.C_Str() /*,p_string.length*/);
-	return name;
-}
-
-Ref<Animation> EditorSceneImporterAssimp::import_animation(const String &p_path, uint32_t p_flags, int p_bake_fps) {
-	return Ref<Animation>();
-}
-
-const Transform EditorSceneImporterAssimp::_assimp_matrix_transform(const aiMatrix4x4 p_matrix) {
-	aiMatrix4x4 matrix = p_matrix;
-	Transform xform;
-	//xform.set(matrix.a1, matrix.b1, matrix.c1, matrix.a2, matrix.b2, matrix.c2, matrix.a3, matrix.b3, matrix.c3, matrix.a4, matrix.b4, matrix.c4);
-	xform.set(matrix.a1, matrix.a2, matrix.a3, matrix.b1, matrix.b2, matrix.b3, matrix.c1, matrix.c2, matrix.c3, matrix.a4, matrix.b4, matrix.c4);
-	return xform;
 }
