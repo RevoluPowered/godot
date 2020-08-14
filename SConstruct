f7ac82eaf071d895bce590b1a55f7165ae456418
Gordon MacPherson <gordon@gordonite.tech>
2020-08-15 00:44:33 +0100

    experiment 01

--- SConstruct
+++ SConstruct
@@ -245,11 +245,11 @@ if env_base["target"] == "debug":
     # will properly be rebuilt. As such, we only enable them for debug (dev) builds, not release.
 
     # Use exact timestamp matches by default.
-    Decider('timestamp-match')
+    Decider("timestamp-match")
 
     # To decide whether to rebuild a file, use the MD5 sum only if the timestamp has changed.
     # http://scons.org/doc/production/HTML/scons-user/ch06.html#idm139837621851792
-    env_base.Decider('timestamp-match')
+    env_base.Decider("timestamp-match")
     # Use cached implicit dependencies by default. Can be overridden by specifying `--implicit-deps-changed` in the command line.
     # http://scons.org/doc/production/HTML/scons-user/ch06s04.html
     env_base.SetOption("implicit_cache", 1)
--- methods.py
+++ methods.py
@@ -57,57 +57,7 @@ def add_module_version_string(self, s):
 
 
 def update_version(module_version_string=""):
-
-    build_name = "custom_build"
-    if os.getenv("BUILD_NAME") != None:
-        build_name = os.getenv("BUILD_NAME")
-        print("Using custom build name: " + build_name)
-
-    import version
-
-    # NOTE: It is safe to generate this file here, since this is still executed serially
-    f = open("core/version_generated.gen.h", "w")
-    f.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-    f.write("#ifndef VERSION_GENERATED_GEN_H\n")
-    f.write("#define VERSION_GENERATED_GEN_H\n")
-    f.write('#define VERSION_SHORT_NAME "' + str(version.short_name) + '"\n')
-    f.write('#define VERSION_NAME "' + str(version.name) + '"\n')
-    f.write("#define VERSION_MAJOR " + str(version.major) + "\n")
-    f.write("#define VERSION_MINOR " + str(version.minor) + "\n")
-    f.write("#define VERSION_PATCH " + str(version.patch) + "\n")
-    f.write('#define VERSION_STATUS "' + str(version.status) + '"\n')
-    f.write('#define VERSION_BUILD "' + str(build_name) + '"\n')
-    f.write('#define VERSION_MODULE_CONFIG "' + str(version.module_config) + module_version_string + '"\n')
-    f.write("#define VERSION_YEAR " + str(version.year) + "\n")
-    f.write('#define VERSION_WEBSITE "' + str(version.website) + '"\n')
-    f.write("#endif // VERSION_GENERATED_GEN_H\n")
-    f.close()
-
-    # NOTE: It is safe to generate this file here, since this is still executed serially
-    fhash = open("core/version_hash.gen.h", "w")
-    fhash.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-    fhash.write("#ifndef VERSION_HASH_GEN_H\n")
-    fhash.write("#define VERSION_HASH_GEN_H\n")
-    githash = ""
-    gitfolder = ".git"
-
-    if os.path.isfile(".git"):
-        module_folder = open(".git", "r").readline().strip()
-        if module_folder.startswith("gitdir: "):
-            gitfolder = module_folder[8:]
-
-    if os.path.isfile(os.path.join(gitfolder, "HEAD")):
-        head = open(os.path.join(gitfolder, "HEAD"), "r", encoding="utf8").readline().strip()
-        if head.startswith("ref: "):
-            head = os.path.join(gitfolder, head[5:])
-            if os.path.isfile(head):
-                githash = open(head, "r").readline().strip()
-        else:
-            githash = head
-
-    fhash.write('#define VERSION_HASH "' + githash + '"\n')
-    fhash.write("#endif // VERSION_HASH_GEN_H\n")
-    fhash.close()
+    return
 
 
 def parse_cg_file(fname, uniforms, sizes, conditionals):
@@ -165,59 +115,7 @@ def is_module(path):
 
 
 def write_modules(module_list):
-    includes_cpp = ""
-    preregister_cpp = ""
-    register_cpp = ""
-    unregister_cpp = ""
-
-    for name, path in module_list.items():
-        try:
-            with open(os.path.join(path, "register_types.h")):
-                includes_cpp += '#include "' + path + '/register_types.h"\n'
-                preregister_cpp += "#ifdef MODULE_" + name.upper() + "_ENABLED\n"
-                preregister_cpp += "#ifdef MODULE_" + name.upper() + "_HAS_PREREGISTER\n"
-                preregister_cpp += "\tpreregister_" + name + "_types();\n"
-                preregister_cpp += "#endif\n"
-                preregister_cpp += "#endif\n"
-                register_cpp += "#ifdef MODULE_" + name.upper() + "_ENABLED\n"
-                register_cpp += "\tregister_" + name + "_types();\n"
-                register_cpp += "#endif\n"
-                unregister_cpp += "#ifdef MODULE_" + name.upper() + "_ENABLED\n"
-                unregister_cpp += "\tunregister_" + name + "_types();\n"
-                unregister_cpp += "#endif\n"
-        except IOError:
-            pass
-
-    modules_cpp = """// register_module_types.gen.cpp
-/* THIS FILE IS GENERATED DO NOT EDIT */
-#include "register_module_types.h"
-
-#include "modules/modules_enabled.gen.h"
-
-%s
-
-void preregister_module_types() {
-%s
-}
-
-void register_module_types() {
-%s
-}
-
-void unregister_module_types() {
-%s
-}
-""" % (
-        includes_cpp,
-        preregister_cpp,
-        register_cpp,
-        unregister_cpp,
-    )
-
-    # NOTE: It is safe to generate this file here, since this is still executed serially
-    with open("modules/register_module_types.gen.cpp", "w") as f:
-        f.write(modules_cpp)
-
+    return
 
 def convert_custom_modules_path(path):
     if not path:
